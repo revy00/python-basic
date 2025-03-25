34dc2febb7a71681f2ec3af7d8513ff215acdb5b
Md.Ayman Zahi <154406121+revy00@users.noreply.github.com>
2025-03-25 21:22:08 +0600

    Create 01_variables.py

---
+++ 01_variables.py
@@ -0,0 +1,21 @@
+#integer variables
+a=10
+b=20
+
+#float variables
+x=3.1416
+
+#string
+name="Md.Ayman Zahi"
+
+#boolean variable
+is_active = True
+
+#list variable
+series = ["The Walking Dead" , "Dexter" , "Breaking Bad" , "One Piece"]
+
+# Tuple variable
+coordinates = (10.5, 20.3)
+
+# Dictionary variable
+person = {"name": "Revy", "age": "XX", "city": "Unknown"}
