455684f7c7f4f52e3da26ec856d69a7682a52942
Md.Ayman Zahi <154406121+revy00@users.noreply.github.com>
2025-03-25 21:50:36 +0600

    Update 1.Name_input_program.py

--- 1.Name_input_program.py
+++ 1.Name_input_program.py
@@ -1,2 +1,8 @@
+#simple_input.1
 name = input("what is your name? ")
 print("Yo"+name)
+
+#simple_input.2
+name = input("What is your name? ")
+fav_series = input("Fav series name? ")
+print(name+ "likes" + fav_series)
