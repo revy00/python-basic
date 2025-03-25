1a8d1fe2f50d3428a8b6d334cf719cbd218349b2
Md.Ayman Zahi <154406121+revy00@users.noreply.github.com>
2025-03-25 21:34:31 +0600

    Create 03_conditions.py

---
+++ 03_conditions.py
@@ -0,0 +1,24 @@
+# Conditional Statements
+
+x=10
+y=32
+
+#if statement
+if x > y:
+  print("x is greater than b")
+
+#if-else statement
+if x=10:
+  print("value of x is 10")
+else
+  print("null")
+
+#if-elif-else statement
+num=12345
+
+if num>0:
+ print("The number is valid")
+elif num<0:
+ print("not valid")
+else
+ print("Dragon Ball Z")
